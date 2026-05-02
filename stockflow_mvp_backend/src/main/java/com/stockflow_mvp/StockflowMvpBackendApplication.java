f5b917b154b7d8cd975ea2a4782f20e9876dffb3
aanandsinghyadav <anandsinghyadav9092@gmail.com>
2026-05-02 05:36:03 +0530

    chore: initialize Spring Boot project with Gradle and base dependencies

---
+++ stockflow_mvp_backend/src/main/java/com/stockflow_mvp/StockflowMvpBackendApplication.java
@@ -0,0 +1,13 @@
+package com.stockflow_mvp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockflowMvpBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockflowMvpBackendApplication.class, args);
+	}
+
+}
